d60bd8774be7c9126a944a5437686a7bbd738b8d
Michael A Nowiszewski <micski@gmail.com>
2018-06-04 10:42:13 +0000

    update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /*  global $
     global ftoi
 NOTE: Turn off alerts in editor */
- 
 $(document).ready(function() {
     $.ajax({
      //method: "GET",
@@ -16,23 +15,53 @@ $(document).ready(function() {
             source.innerHTML = data.sources[i].name;
             document.getElementById('selection').appendChild(source);
             
+            
          }
      }
   }
  
- 
- 
- 
+//DO NOT TOUCH 
   })
-   $('#source').submit( function(event) {
+  
+  
+  
+  function XYZ (headlines){
+  $.ajax({
+     //method: "GET",
+     url: "https://newsapi.org/v2/top-headlines",
+     data: { category: "sport", country: "us", language: "en", apiKey: "299724b477974817929291d2fcbe15d4"},
+     success: function(headline){
+    
+        for (var x = 0; x < headline.articles.length; x++) {
+             var headlines = document.createElement("P")
+             headline.innerHTML = headline.articles[x].title;
+             document.getElementById("headline").appendChild(headlines)
+             console.log(headline)
+             
+        }
+     }
+
+            
+  //
+  $('#source').submit( function(event) {
       event.preventDefault();
       alert(document.getElementById('selection').value)
+      displayIT(document.)
+      //Top headlines from source selected - 
       
-   });
-})
-
-
-
+      //Function to display headlines with an ajax call
+      
+      // use ID - reach out to top headlines - display "title"
+      
+  });
+         
+     });
+      
+  });
+    
+});
+});
+       //
 
 
 
